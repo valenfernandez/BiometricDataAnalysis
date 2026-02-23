df27127d5ba92c27f5a1fc172c8cbfe376b55cd8
valenfernandez <valen.fernandez.montenegro@gmail.com>
2026-02-23 12:13:29 -0300

    add: backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import compare
 
 
@@ -8,6 +9,13 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(compare.router, prefix="/compare", tags=["Compare"])
 
