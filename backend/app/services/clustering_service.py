b3e7d1c9ebc0982ab532c2c86900340b436eb9df
valenfernandez <valen.fernandez.montenegro@gmail.com>
2026-03-04 15:30:39 -0300

    Update clustering_service.py

--- backend/app/services/clustering_service.py
+++ backend/app/services/clustering_service.py
@@ -5,8 +5,9 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import PCA
 from app.services.compare_service import decode_image, extract_embedding
 from app.core.model_loader import get_models
+from sklearn.metrics.pairwise import cosine_distances
 
-async def cluster_faces(images: list[UploadFile], threshold: float = 0.6):
+async def cluster_faces(images: list[UploadFile], threshold: float = 0.12):
 
     if len(images) > 10:
         raise ValueError("Maximum 10 images allowed")
@@ -23,13 +24,15 @@ async def cluster_faces(images: list[UploadFile], threshold: float = 0.6):
             raise ValueError(f"Invalid image: {image.filename}")
         
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         faces = face_detector(gray)
 
         for idx, face in enumerate(faces):
             shape = shape_predictor(gray, face)
-            embedding = face_rec_model.compute_face_descriptor(img, shape)
+            embedding = face_rec_model.compute_face_descriptor(rgb, shape)
+            # embedding = face_rec_model.compute_face_descriptor(img, shape)
             embeddings.append(np.array(embedding))
-            image_names.append(f"{image.filename}_face_{idx}")  #TODO: FIX if name has _face_
+            image_names.append(f"{image.filename}_face_{idx}")  
             face_metadata.append({
                 "image": image.filename,
                 "face_index": idx
@@ -47,7 +50,7 @@ async def cluster_faces(images: list[UploadFile], threshold: float = 0.6):
     else:
         clustering = AgglomerativeClustering(
             n_clusters=None,
-            metric="euclidean",
+            metric="cosine",
             linkage="average",
             distance_threshold=threshold
         )
@@ -57,7 +60,7 @@ async def cluster_faces(images: list[UploadFile], threshold: float = 0.6):
         reduced = pca.fit_transform(embeddings)
 
     faces_output = []
-    
+
     for meta, label, coords in zip(face_metadata, labels, reduced):
         faces_output.append({
             "image": meta["image"],
@@ -66,6 +69,14 @@ async def cluster_faces(images: list[UploadFile], threshold: float = 0.6):
             "x": float(coords[0]),
             "y": float(coords[1])
         })
+    
+
+
+    dist_matrix = cosine_distances(embeddings)
+
+    print("Cosine distance matrix:")
+    print(np.round(dist_matrix, 3))
+
 
     return {
         "total_images": len(images),
